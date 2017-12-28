0591a0817635cb1ddd39673c0c9642586e5ac79f
Christopher Dorst <chris@dorst.email>
2017-12-27 19:47:36 -0600

    Adding UserInfoSetter

---
+++ DevOps.VersionControl.Functions.SetGitRepositoryUserInfo/UserInfoSetter.cs
@@ -0,0 +1,16 @@
+﻿using DevOps.VersionControl.Functions.RunGitConfigCommand.GitConfigCommandRunner;
+
+namespace DevOps.VersionControl.Functions.SetGitRepositoryUserInfo
+{
+    public static class UserInfoSetter
+    {
+        private const string UserEmail = "user.email";
+        private const string UserName = "user.name";
+
+        public static void SetUserInfo(string repositoryDirectory, string username, string email)
+        {
+            Config(repositoryDirectory, UserEmail, email);
+            Config(repositoryDirectory, UserName, username);
+        }
+    }
+}
